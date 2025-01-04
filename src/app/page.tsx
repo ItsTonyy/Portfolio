db3cda3d0ae600cc92dddaec7b10d23749a29550
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-01-04 00:23:40 -0300

    fix: dark mode not activating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
  
-import { AuroraBackground } from "@/components/ui/aurora-background";
-import { motion } from "framer-motion";
 import React from "react";
+import { motion } from "framer-motion";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { ModeToggle } from "@/components/modeToggle";
 
 export default function Home() {
   return (
@@ -17,16 +18,12 @@ export default function Home() {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
+        <ModeToggle />
         <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
           Background lights are cool you know.
         </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
-        </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
-        </button>
       </motion.div>
+      
     </AuroraBackground>
   );
 }
